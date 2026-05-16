0cfd90ffa2b5e5ac43aeb715ed6e199a115c05ec
MuhammadUmar-dotnetdev <reft670@gmail.com>
2026-05-16 17:05:08 +0500

    Added print(wwooww)

--- add.py
+++ add.py
@@ -10,7 +10,11 @@ result = addvar1 * addvar2
 print(result)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 print("main")
 =======
 print("")
 >>>>>>> 2bd1f06 (Added print())
+=======
+print("wwooww")
+>>>>>>> 07474a7 (Added print(wwooww))

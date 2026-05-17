4ea6fed351c18adbe2aab83e050fc8036b0ad6b9
MuhammadUmar-dotnetdev <reft670@gmail.com>
2026-05-17 22:25:57 +0500

    function added

--- add.py
+++ add.py
@@ -1,3 +1,6 @@
+def add(num1, num2):
+    print(num1 * num2)
+
 print("add.py added")
 
 print("add.py modified")
@@ -8,3 +11,5 @@ addvar2 = 11
 result = addvar1 * addvar2
 
 print(result)
+
+add(addvar1, addvar2)
